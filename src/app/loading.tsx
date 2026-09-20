d252c3419fa0499049210312dd9aad3c149aa501
Ayman Emoo <40736903+ayman202@users.noreply.github.com>
2026-09-21 01:54:43 +0300

    s

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center py-24">
-      <div className="w-8 h-8 border-2 border-accent border-t-transparent rounded-full animate-spin" />
-    </div>
-  );
-}
