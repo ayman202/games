c1fb464bbe76e7f09d1c9c32f6783a6d9cc784f6
Ayman Emoo <40736903+ayman202@users.noreply.github.com>
2026-09-21 13:19:30 +0300

    dd

--- src/app/ads.txt/route.ts
+++
@@ -1,8 +0,0 @@
-import { getSettings } from "@/lib/settings";
-
-export async function GET() {
-  const settings = await getSettings();
-  return new Response(settings.adsTxtContent || "", {
-    headers: { "Content-Type": "text/plain" },
-  });
-}
